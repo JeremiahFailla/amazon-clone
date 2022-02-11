99a77afd3354fb4c9659e4d841f3a6c5ada2991c
JeremiahFailla <jpf405@gmail.com>
2022-02-11 16:16:41 -0500

    finished firebase logic along with hosting on firebase

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // API (cloud function) URL
   baseURL: "http://localhost:5001/clone-a3b0c/us-central1/api",
-  // baseURL: ""
+  // baseURL: "https://us-central1-clone-a3b0c.cloudfunctions.net/api",
 });
 
 export default instance;
